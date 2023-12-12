a7bb988b0cdfe25401a68d73213c9e5ac4e41dff
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 11:19:15 -0500

    Update bisection_test4.py
    Fixed too many inputs in assert

--- bisection_test4.py
+++ bisection_test4.py
@@ -6,4 +6,4 @@ def example_func(x):
     return 1-x**2
 
 def test_bisection():
-    assert bisection.bisection_algorithm(example_func, ,0, 16, .01) == (1.0, 0.0, 4)
+    assert bisection.bisection_algorithm(example_func, 0, 16, .01) == (1.0, 0.0, 4)
