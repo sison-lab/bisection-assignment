3e85a116cd4a6d09cc9e4c24e217ce80488fdc33
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 10:45:09 -0500

    Update bisection_test3.py

--- bisection_test3.py
+++ bisection_test3.py
@@ -1,9 +1,9 @@
 import bisection;
 
-# Test whether it will work when solution is right endpoint
+# Test whether it will work when solution is left endpoint
 
 def example_func(x):
   return 1-x**2
 
 def test_bisection():
-    assert bisection.bisection_algorithm(example_func, 1, 2, .01) == (1.734375, -0.008056640625, 7)
+    assert bisection.bisection_algorithm(example_func, 1, 2, .01) == (1, 0, 0)
