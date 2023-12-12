d1b2ddcaa357bb3adb818941ee55f7b9839ea5a9
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 10:25:15 -0500

    Update bisection_test1.py
    Fixed issue by deleting obsolete end of line of code

--- bisection_test1.py
+++ bisection_test1.py
@@ -4,7 +4,7 @@ def example_func(x):
   return 3-x**2
 
 def test_bisection():
-    location, error, number_of_tries = bisection.bisection_algorithm(example_func, 0, 2, .01) == (1.734375, -0.008056640625, 7)
+    location, error, number_of_tries = bisection.bisection_algorithm(example_func, 0, 2, .01)
     assert location == 1.734375
     assert error == -0.008056640625
     assert number_of_tries == 7
