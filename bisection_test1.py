62776f019d603ca185d7cb90aafcb98445d54ee7
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 10:19:20 -0500

    Update bisection_test1.py

--- bisection_test1.py
+++ bisection_test1.py
@@ -4,4 +4,8 @@ def example_func(x):
   return 3-x**2
 
 def test_bisection():
+    location, error, number_of_tries = bisection.bisection_algorithm(example_func, 0, 2, .01) == (1.734375, -0.008056640625, 7)
+    assert location = 1.734375
+    assert error = -0.008056640625
+    assert number_of_tries = 7
     assert bisection.bisection_algorithm(example_func, 0, 2, .01) == (1.734375, -0.008056640625, 7)
