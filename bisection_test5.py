6aab60c83149584a8b3695c3a243f083211b9a67
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 10:48:02 -0500

    Update bisection_test5.py

--- bisection_test5.py
+++ bisection_test5.py
@@ -3,7 +3,7 @@ import bisection;
 # Test whether it will terminate if it can't find the root
 
 def example_func(x):
-  return 3 + x**2
+    return 3 + x**2
 
 def test_bisection():
-    assert bisection.bisection_algorithm(example_func, 0, 1, .01) == (1.734375, -0.008056640625, 7)
+    assert bisection.bisection_algorithm(example_func, 0, 1, .01) == "Could Not Locate Root"
