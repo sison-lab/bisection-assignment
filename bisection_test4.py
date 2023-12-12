ff633031dba981dc4f7b41c90baae2201adbd622
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 10:47:16 -0500

    Update bisection_test4.py

--- bisection_test4.py
+++ bisection_test4.py
@@ -3,7 +3,7 @@ import bisection;
 # Test whether it will work when solution is found exactly in the interior
 
 def example_func(x):
-  return 1-x**3
+    return 1-x**2
 
 def test_bisection():
-    assert bisection.bisection_algorithm(example_func, 0, 4, .01) == (1.734375, -0.008056640625, 7)
+    assert bisection.bisection_algorithm(example_func, ,0, 16, .01) == (1.0, 0.0, 4)
