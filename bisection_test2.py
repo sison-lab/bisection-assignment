73a0fd8a8a4c90adc98abb0dc5330a797044d043
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 10:48:44 -0500

    Update bisection_test2.py

--- bisection_test2.py
+++ bisection_test2.py
@@ -3,7 +3,7 @@ import bisection;
 # Test whether it will work when solution is right endpoint
 
 def example_func(x):
-  return 1-x**2
+    return 1-x**2
 
 def test_bisection():
     assert bisection.bisection_algorithm(example_func, 0, 1, .01) == (1, 0, 0)
