d149b7f895a015b22faf9a917a0ae54810ec2d3b
sison-lab <153555712+sison-lab@users.noreply.github.com>
2023-12-12 09:42:45 -0500

    Update bisection_test.py
    Fixed issue where it was calling hello.bisection_algorithm instead of bisection.bisection_algorithm

--- bisection_test.py
+++ bisection_test.py
@@ -4,4 +4,4 @@ def example_func(x):
   return 3-x**2
 
 def test_bisection():
-    assert hello.bisection_algorithm(example_func, 0, 2, .01) == (1.734375, -0.008056640625, 7)
+    assert bisection.bisection_algorithm(example_func, 0, 2, .01) == (1.734375, -0.008056640625, 7)
